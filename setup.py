745a4bbfc47a0dd7ef948dc08a9f58e00ecae11d
lalondesteve <lalondesteve@gmail.com>
2020-08-27 19:48:15 -0400

    Setting up setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
     install_requires=REQUIRED,
-    extras_require=EXTRAS,
     include_package_data=True,
     license='MIT',
     classifiers=[
