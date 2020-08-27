7cf2ef2c2b0e5f2258a93c7fb558d52bc794edda
lalondesteve <lalondesteve@gmail.com>
2020-08-27 19:47:57 -0400

    Setting up setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,16 @@ REQUIRED = [
     'requests', 'python-dotenv'
 ]
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
 setup(
     name=NAME,
     version=VERSION,
