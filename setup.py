cceb43ecf0e1adf0a69e6c4170b96dbaa90da4cd
lalondesteve <lalondesteve@gmail.com>
2020-09-06 21:03:27 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import io
-import os
-import sys
-from shutil import rmtree
-
-from setuptools import find_packages, setup
+import setuptools
 
 # Package meta-data.
 NAME = 'SPOClient'
@@ -20,38 +13,30 @@ REQUIRED = [
     'requests', 'wheel', 'python-dotenv'
 ]
 
-here = os.path.abspath(os.path.dirname(__file__))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=['SPOClient'],
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-    install_requires=REQUIRED,
-    include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6'
-    ],
-)
+setuptools. \
+    setup(name=NAME,
+          version='0.1',
+          description=DESCRIPTION,
+          long_description=long_description,
+          long_description_content_type='text/markdown',
+          url=URL,
+          author=AUTHOR,
+          author_email=EMAIL,
+          license='MIT',
+          packages=setuptools.find_packages(),
+          install_requires=REQUIRED,
+          include_package_data=True,
+          zip_safe=False,
+          classifiers=[
+              # Trove classifiers
+              # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+              'License :: OSI Approved :: MIT License',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.8',
+              'Development Status :: 3 - Alpha',
+          ],
+          )
